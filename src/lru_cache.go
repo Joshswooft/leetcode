076f196a31475a157283d07a7fefc977d5ffb221
Josh Dando <dandobusiness@gmail.com>
2023-09-10 12:33:20 +0100

    feat: lru cache solution

--- src/lru_cache.go
+++ src/lru_cache.go
@@ -1,6 +1,8 @@
 package main
 
-import "errors"
+import (
+	"errors"
+)
 
 /**
 
@@ -14,12 +16,218 @@ The cache has two main operations:
 Get(key int) -> Gets a value in the cache by it's key. If the value doesn't exist we return -1 (for the sake of this shitty leetcode test). When we get the given value we have to update it so it's at the front of the cache (as it's now the most recently used)
 Put(key int, value int) -> If the item doesn't already exist in the cache then we add the key-value pair. If the number of keys exceeds the capacity then we have to evict the least recently used key. If the item already exists then update it.
 
-
+Get and Put must be O(1) time complexity
 
 */
 
+type KeyPair struct {
+	Key int
+	Val int
+}
+
+type DoubleLinkedListNode struct {
+	Val  KeyPair
+	Next *DoubleLinkedListNode
+	Prev *DoubleLinkedListNode
+}
+
+type DoubleLinkedList struct {
+	head   DoubleLinkedListNode
+	length int
+}
+
+// Inits / clears list
+func (l *DoubleLinkedList) Init() *DoubleLinkedList {
+	l.head.Next = &l.head
+	l.head.Prev = &l.head
+	l.length = 0
+	return l
+}
+
+// Makes a new list ready for use
+func NewList() *DoubleLinkedList {
+	return new(DoubleLinkedList).Init()
+}
+
+// Inserts element at the end of the list
+func (l *DoubleLinkedList) Insert(value KeyPair) {
+
+	tail := l.Tail()
+
+	node := DoubleLinkedListNode{
+		Val:  value,
+		Next: &l.head,
+		Prev: tail,
+	}
+
+	// head points to new tail
+	l.head.Prev = &node
+
+	// previous tail now points to node
+	tail.Next = &node
+
+	// we only have one value so it points to itself
+	if l.head.Val.Key == l.head.Next.Val.Key {
+		l.head.Next = &node
+	}
+
+	l.length = l.length + 1
+}
+
+// returns the head node
+func (l *DoubleLinkedList) Head() *DoubleLinkedListNode {
+	if l == nil || l.head.Next == nil {
+		return nil
+	}
+	return l.head.Next
+}
+
+// returns the tail node
+func (l *DoubleLinkedList) Tail() *DoubleLinkedListNode {
+	if l == nil || l.head.Prev == nil {
+		return nil
+	}
+	return l.head.Prev
+}
+
+func (l *DoubleLinkedList) InsertAtHead(node *DoubleLinkedListNode) {
+	firstNode := l.Head()
+
+	// update the existing node
+	if firstNode.Val.Key == node.Val.Key {
+		firstNode.Next = node.Next
+		firstNode.Prev = node.Prev
+		firstNode.Val = node.Val
+		return
+	}
+
+	node.Prev = &l.head
+
+	// update the oldHead to point to our new node instead of the dummy head node
+	firstNode.Prev = node
+	node.Next = firstNode
+
+	l.head.Next = node
+
+	if l.length == 0 {
+		// 1st node being inserted so head == tail
+		l.head.Prev = node
+	}
+
+	l.length++
+
+}
+
+// Removes a node from the linked list
+func (l *DoubleLinkedList) Remove(node *DoubleLinkedListNode) {
+	if node == nil || l.length == 0 {
+		return
+	}
+
+	if node == nil {
+		return
+	}
+
+	if node.Next != nil {
+		node.Next.Prev = node.Prev
+	}
+
+	if node.Prev != nil {
+		node.Prev.Next = node.Next
+	}
+
+	// head is not a pointer so we have to manually do it here
+	if l.head.Next != nil {
+		if l.head.Next.Val.Key == node.Val.Key {
+			l.head.Next = node.Next
+		}
+	}
+
+	if l.head.Prev != nil {
+		if l.head.Prev.Val.Key == node.Val.Key {
+			l.head.Prev = node.Prev
+		}
+	}
+
+	// *delete it by assigning as nil
+	node = nil
+
+	l.length--
+}
+
+func (l *DoubleLinkedList) FindNode(key int) *DoubleLinkedListNode {
+	return l.head.findNode(key, 0, l.length)
+
+}
+
+// returns a node with matching key or nil
+func (l *DoubleLinkedListNode) findNode(key int, counter int, max int) *DoubleLinkedListNode {
+
+	if l.Val.Key == key || l == nil {
+		return l
+	}
+
+	if counter >= max {
+		return nil
+	}
+
+	if l.Next != nil {
+		return l.Next.findNode(key, counter+1, max)
+	}
+
+	return nil
+
+}
+
+const forwards = "forwards"
+const backwards = "backwards"
+
+// returns all the KeyPairs in the list
+func (l *DoubleLinkedListNode) travese(arr []KeyPair, head DoubleLinkedListNode, counter int, maxSize int, direction string) []KeyPair {
+
+	if counter >= maxSize {
+		return arr
+	}
+
+	// we've encounted a cycle - assuming here that the keys are unique
+	if l.Val.Key == head.Val.Key {
+		return arr
+	}
+
+	arr = append(arr, l.Val)
+	if l.Next != nil && direction == forwards {
+		return l.Next.travese(arr, head, counter+1, maxSize, direction)
+	}
+
+	if l.Prev != nil && direction == backwards {
+		return l.Prev.travese(arr, head, counter+1, maxSize, direction)
+	}
+
+	return arr
+}
+
+// ToList converts linked list to an array of its values
+func (l *DoubleLinkedList) ToList() []KeyPair {
+	var values []KeyPair
+	if l == nil || l.length == 0 || l.head.Next == nil {
+		return []KeyPair{}
+	}
+	return l.head.Next.travese(values, l.head, 0, l.length, forwards)
+}
+
+// GetReverseList converts linked list into array of its values but going backwards from tail -> head
+func (l *DoubleLinkedList) GetReverseList() []KeyPair {
+	var values []KeyPair
+	if l == nil || l.length == 0 || l.head.Prev == nil {
+		return []KeyPair{}
+	}
+	return l.head.Prev.travese(values, l.head, 0, l.length, backwards)
+}
+
 type lruCache struct {
 	capacity int
+	values   map[int]*DoubleLinkedListNode // map of key to pointer of nodes
+	list     DoubleLinkedList              // actual list
 }
 
 var errInvalidCapacity = errors.New("capacity has to be a positive number")
@@ -31,15 +239,80 @@ func NewLRUCache(capacity int) (*lruCache, error) {
 
 	return &lruCache{
 		capacity: capacity,
+		values:   make(map[int]*DoubleLinkedListNode, capacity),
+		list:     *NewList(),
 	}, nil
 }
 
-// Get - gets the value of a cache by it's key. If the key doesn't exist we return -1
-func (c *lruCache) Get(key int) int {
-	return -1
+// leetcode shit
+type LRUCache = lruCache
+
+// this is for leetcode use
+func Constructor(capacity int) LRUCache {
+	return lruCache{
+		capacity: capacity,
+		values:   make(map[int]*DoubleLinkedListNode, capacity),
+		list:     *NewList(),
+	}
 }
 
-// If the item doesn't already exist in the cache then we add the key-value pair. If the number of keys exceeds the capacity then we have to evict the least recently used key. If the item already exists then update it.
+// Get - gets the value of a cache by it's key. If the key doesn't exist we return -1
+// When we get the given value we have to update it so it's at the front of the cache (as it's now the most recently used)
+func (c *lruCache) Get(key int) int {
+	node, ok := c.values[key]
+
+	if !ok {
+		return -1
+	}
+
+	// Get can be described as simply Removing the current element then doing an insertion at the head
+
+	c.list.Remove(node)
+	c.list.InsertAtHead(node)
+
+	return node.Val.Val
+}
+
+// If the item doesn't already exist in the cache then we add the key-value pair.
+// internally when we put a value it's inserted at the head of the stack
+// If the number of keys exceeds the capacity then we have to evict the least recently used key.
+// If the item already exists then update it.
 func (c *lruCache) Put(key int, value int) {
+
+	// Put is slightly more difficult theres a few scenarios to consider:
+
+	// 1. cache is not full and data doesn't already exist in cache
+	// we create a new node and simply insert at head
+
+	// 2. cache is not full and data already exists in cache
+	// we remove the current item then insert it at the head
+
+	// 3. cache is full and data doesn't already exist in cache
+	// we remove the tail then we create a new node to insert at head
+
+	// 4. cache is full and data already exists in cache
+	// we remove current item and re-insert at the head
+
+	node, exists := c.values[key]
+
+	if c.list.length >= c.capacity && !exists {
+		// cache is full
+		tail := c.list.Tail()
+		c.list.Remove(tail)
+		delete(c.values, tail.Val.Key) // remove the reference to the node
+	}
+
+	if exists {
+		c.list.Remove(node)
+		node.Val.Val = value
+		c.list.InsertAtHead(node)
+	} else {
+		// node doesn't exist
+		newNode := DoubleLinkedListNode{
+			Val: KeyPair{Key: key, Val: value},
+		}
+		c.list.InsertAtHead(&newNode)
+		c.values[key] = &newNode
+	}
 
 }
--- src/lru_cache_test.go
+++ src/lru_cache_test.go
@@ -1,9 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
+
+func TestCacheScenarios(t *testing.T) {
+
+	checkList := func(cache *lruCache, expectedForwardList []KeyPair) {
+		list := cache.list.ToList()
+		reverseList := cache.list.GetReverseList()
+
+		var j int
+		var expectedReverseList = make([]KeyPair, len(expectedForwardList))
+
+		for i := len(expectedForwardList) - 1; i >= 0; i-- {
+			expectedReverseList[j] = expectedForwardList[i]
+			j++
+		}
+
+		if !reflect.DeepEqual(list, expectedForwardList) {
+			t.Errorf("unexpected list after Put(1, 1), got: %v, want: %v", list, expectedForwardList)
+		}
+
+		if !reflect.DeepEqual(reverseList, expectedReverseList) {
+			t.Errorf("unexpected reverse list after Put(1, 1). got: %v, want: %v", reverseList, expectedReverseList)
+		}
+
+	}
+
+	cache, _ := NewLRUCache(3)
+
+	cache.Put(1, 1)
+	checkList(cache, []KeyPair{{1, 1}})
+
+	cache.Put(2, 2)
+	checkList(cache, []KeyPair{{2, 2}, {1, 1}})
+
+	cache.Put(3, 3)
+	checkList(cache, []KeyPair{{3, 3}, {2, 2}, {1, 1}})
+
+	got := cache.Get(0)
+	if got != -1 {
+		t.Errorf("got a value in the cache that doesn't exist")
+	}
+
+	// check cache eviction of value 4
+	cache.Put(4, 4)
+	checkList(cache, []KeyPair{{4, 4}, {3, 3}, {2, 2}})
+
+	// check update of cache
+	cache.Put(2, 5)
+	checkList(cache, []KeyPair{{2, 5}, {4, 4}, {3, 3}})
+
+	node := cache.Get(3)
+
+	if node != 3 {
+		t.Error("failed to get correct value")
+	}
+
+	checkList(cache, []KeyPair{{3, 3}, {2, 5}, {4, 4}})
+
+	cache.Put(1, 1)
+	checkList(cache, []KeyPair{{1, 1}, {3, 3}, {2, 5}})
+
+}
 
 func TestNewLRUCache(t *testing.T) {
 	type args struct {
@@ -36,14 +98,602 @@ func TestNewLRUCache(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewLRUCache(tt.args.capacity)
+			_, err := NewLRUCache(tt.args.capacity)
 			if err != tt.expErr {
 				t.Errorf("NewLRUCache() error = %v, wantErr %v", err, tt.expErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewLRUCache() = %v, want %v", got, tt.want)
+		})
+	}
+}
+
+func Test_lruCache_Get(t *testing.T) {
+
+	type args struct {
+		key int
+	}
+
+	tests := []struct {
+		name      string
+		initCache func() *lruCache
+		args      args
+		want      int
+	}{
+		{
+			name: "key doesnt exist in cache",
+			initCache: func() *lruCache {
+				cache, err := NewLRUCache(5)
+				if err != nil {
+					t.Error("failed to create cache ", err)
+				}
+				return cache
+			},
+			args: args{
+				key: 1,
+			},
+			want: -1,
+		},
+		{
+			name: "gets key-value from the cache with given key",
+			initCache: func() *lruCache {
+				cache, err := NewLRUCache(5)
+				if err != nil {
+					t.Error("failed to create cache ", err)
+				}
+
+				cache.Put(0, 5)
+				cache.Put(1, 10)
+				cache.Put(2, 15)
+
+				return cache
+			},
+			args: args{
+				key: 1,
+			},
+			want: 10,
+		},
+		{
+			name: "updates the key-value to most recently used",
+			initCache: func() *lruCache {
+				cache, err := NewLRUCache(5)
+				if err != nil {
+					t.Error("failed to create cache ", err)
+				}
+
+				cache.Put(1, 1)
+				cache.Put(2, 0)
+				cache.Put(5, 5)
+
+				return cache
+			},
+			args: args{
+				key: 2,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := tt.initCache()
+			got := c.Get(tt.args.key)
+
+			if got != tt.want {
+				t.Errorf("lruCache.Get() = %v, want %v", got, tt.want)
 			}
+
+			head := c.list.Head()
+
+			if got != -1 && head != nil && head.Val.Key != tt.args.key {
+				t.Error("cache didnt update key to most recently used")
+			}
+
+		})
+	}
+}
+
+func Test_lruCache_Put(t *testing.T) {
+
+	type args struct {
+		key   int
+		value int
+	}
+	tests := []struct {
+		name              string
+		initCache         func() *lruCache
+		args              args
+		expectedKeyPairs  []KeyPair
+		expectedCacheSize int
+		expectedCacheKeys []int
+	}{
+		{
+			name: "Puts a new value into empty cache",
+			initCache: func() *lruCache {
+				cache, err := NewLRUCache(1)
+				if err != nil {
+					t.Error("failed to init cache, ", err)
+				}
+				return cache
+			},
+			args: args{
+				key:   1,
+				value: 2,
+			},
+			expectedKeyPairs:  []KeyPair{{Key: 1, Val: 2}},
+			expectedCacheSize: 1,
+			expectedCacheKeys: []int{1},
+		},
+		{
+			name: "Updates existing key with value in the cache",
+			initCache: func() *lruCache {
+
+				cache, _ := NewLRUCache(5)
+
+				cache.Put(1, 0)
+				cache.Put(10, 4)
+				return cache
+			},
+			args: args{
+				key:   1,
+				value: 2,
+			},
+			expectedKeyPairs:  []KeyPair{{Key: 1, Val: 2}, {Key: 10, Val: 4}},
+			expectedCacheSize: 2,
+			expectedCacheKeys: []int{1, 10},
+		},
+		{
+			name: "Adds a new key-val pair to a cache with max capacity, since cache is at max capacity it will remove the least recently used",
+			initCache: func() *lruCache {
+				cache, err := NewLRUCache(1)
+
+				if err != nil {
+					t.Error("failed to init cache, ", err)
+				}
+
+				// fills up the cache
+				cache.list.Insert(KeyPair{Key: 1, Val: 1})
+				return cache
+			},
+			args: args{
+				key:   3,
+				value: 6,
+			},
+			expectedKeyPairs:  []KeyPair{{Key: 3, Val: 6}},
+			expectedCacheSize: 1,
+			expectedCacheKeys: []int{3},
+		},
+		{
+			name: "updates an existing node in a max capacity cache",
+			initCache: func() *lruCache {
+				cache, err := NewLRUCache(3)
+
+				node1 := DoubleLinkedListNode{
+					Val: KeyPair{Key: 1, Val: 1},
+				}
+
+				node2 := DoubleLinkedListNode{
+					Val: KeyPair{Key: 2, Val: 5},
+				}
+
+				node3 := DoubleLinkedListNode{
+					Val: KeyPair{Key: 6, Val: 9},
+				}
+
+				cache.list.InsertAtHead(&node3)
+				cache.list.InsertAtHead(&node2)
+				cache.list.InsertAtHead(&node1)
+
+				// 1 -> 2 -> 6
+
+				cache.values = map[int]*DoubleLinkedListNode{
+					1: &node1,
+					2: &node2,
+					3: &node3,
+				}
+
+				if err != nil {
+					t.Error("failed to init cache, ", err)
+				}
+
+				return cache
+			},
+			args: args{
+				key:   2,
+				value: 3,
+			},
+			expectedKeyPairs:  []KeyPair{{Key: 2, Val: 3}, {Key: 1, Val: 1}, {Key: 6, Val: 9}},
+			expectedCacheSize: 3,
+			expectedCacheKeys: []int{2, 1, 6},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := tt.initCache()
+			c.Put(tt.args.key, tt.args.value)
+
+			if c.list.length != tt.expectedCacheSize {
+				t.Errorf("cache size is unexpected. got: %v, want: %v", c.list.length, tt.expectedCacheSize)
+			}
+
+			// TODO: check keys are correct (ideally we would like to check nodes as well but I'm not sure how to do considering pointers)
+			keys := make([]int, len(c.values))
+			i := 0
+			for k := range c.values {
+				keys[i] = k
+				i++
+			}
+			fmt.Println(keys)
+
+			keyPairs := c.list.ToList()
+
+			if !reflect.DeepEqual(keyPairs, tt.expectedKeyPairs) {
+				t.Errorf("expected values: %v, but got: %v", tt.expectedKeyPairs, keyPairs)
+			}
+		})
+	}
+}
+
+func TestDoubleLinkedList_Insert(t *testing.T) {
+	type fields struct {
+		head   func() DoubleLinkedListNode
+		length int
+	}
+	type args struct {
+		value KeyPair
+	}
+	tests := []struct {
+		name             string
+		fields           fields
+		args             args
+		expectedLength   int
+		expectedElements []KeyPair
+	}{
+		{
+			name: "inserts a new keyvalue pair into an empty list",
+			fields: fields{
+				head: func() DoubleLinkedListNode {
+					return NewList().head
+				},
+				length: NewList().length,
+			},
+			args: args{
+				value: KeyPair{Key: 1, Val: 2},
+			},
+			expectedLength: 1,
+			expectedElements: []KeyPair{
+				{Key: 1, Val: 2},
+			},
+		},
+		{
+			name: "add to existing list",
+			fields: fields{
+				length: 1,
+				head: func() DoubleLinkedListNode {
+					l := NewList()
+
+					l.Insert(KeyPair{1, 1})
+
+					return l.head
+				},
+			},
+			args: args{
+				value: KeyPair{2, 2},
+			},
+			expectedLength:   2,
+			expectedElements: []KeyPair{{1, 1}, {2, 2}},
+		},
+		{
+			name: "daddy spank me",
+			fields: fields{
+				length: 2,
+				head: func() DoubleLinkedListNode {
+					l := NewList()
+
+					l.Insert(KeyPair{1, 1})
+					l.Insert(KeyPair{2, 2})
+
+					// 1 -> 2
+					return l.head
+				},
+			},
+			args: args{
+				value: KeyPair{Key: 3, Val: 3},
+			},
+			expectedLength: 3,
+			expectedElements: []KeyPair{
+				{Key: 1, Val: 1},
+				{Key: 2, Val: 2},
+				{Key: 3, Val: 3},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &DoubleLinkedList{
+				head:   tt.fields.head(),
+				length: tt.fields.length,
+			}
+			l.Insert(tt.args.value)
+
+			// expect that new value is at end of list
+
+			keyPairs := l.ToList()
+
+			reverseList := l.GetReverseList()
+
+			var forwardsList = make([]KeyPair, len(reverseList))
+
+			var j int
+			for i := len(reverseList) - 1; i >= 0; i-- {
+				forwardsList[j] = reverseList[i]
+				j++
+			}
+
+			lastKeyPair := keyPairs[len(keyPairs)-1]
+
+			if lastKeyPair != tt.args.value {
+				t.Errorf("last key pair are not equal! got: %v, want: %v", lastKeyPair, tt.args.value)
+			}
+
+			if !reflect.DeepEqual(keyPairs, tt.expectedElements) {
+				t.Errorf("key pairs are not equal! got: %v, want: %v", keyPairs, tt.expectedElements)
+			}
+
+			if !reflect.DeepEqual(keyPairs, forwardsList) {
+				// this error suggests that the tail pointers aren't being setup correctly
+				t.Errorf("forward list does not equal the same list in reverse. keyPairs: %v, reverseList: %v, forwardsList: %v", keyPairs, reverseList, forwardsList)
+			}
+
+			// expect head.Prev to point to new value
+
+			tail := l.head.Prev
+
+			if tail.Val != tt.args.value {
+				t.Errorf("tail value is incorrect! got: %v, want: %v", tail.Val, tt.args.value)
+			}
+
+			// expect length
+
+			if tt.expectedLength != l.length {
+				t.Errorf("length is not equal got: %v, want: %v", l.length, tt.expectedLength)
+			}
+
+		})
+	}
+}
+
+func TestDoubleLinkedList_Remove(t *testing.T) {
+
+	node := DoubleLinkedListNode{
+		Val: KeyPair{Key: 1, Val: 2},
+	}
+
+	node7 := DoubleLinkedListNode{
+		Val: KeyPair{Key: 7, Val: 7},
+	}
+
+	setup := func() DoubleLinkedListNode {
+		var node1, node2 *DoubleLinkedListNode
+
+		head := NewList().head
+
+		node2 = &DoubleLinkedListNode{
+			Next: &head,
+			Val:  KeyPair{Key: 2, Val: 6},
+		}
+
+		node1 = &DoubleLinkedListNode{
+			Prev: &head,
+			Next: node2,
+			Val:  KeyPair{Key: 1, Val: 7},
+		}
+
+		node2.Prev = node1
+
+		head.Next = node1
+		head.Prev = node2
+
+		return head
+	}
+
+	type fields struct {
+		head   func() DoubleLinkedListNode
+		length int
+	}
+	type args struct {
+		node *DoubleLinkedListNode
+	}
+	tests := []struct {
+		name             string
+		fields           fields
+		args             args
+		expectedLength   int
+		expectedKeyPairs []KeyPair
+	}{
+		{
+			name: "removes from an empty list",
+			fields: fields{
+				head: func() DoubleLinkedListNode {
+					return NewList().head
+				},
+				length: NewList().length,
+			},
+			args: args{
+				node: &DoubleLinkedListNode{
+					Val: KeyPair{Key: 1, Val: 1},
+				},
+			},
+			expectedLength:   0,
+			expectedKeyPairs: []KeyPair{},
+		},
+		{
+			name: "removes 1st and only node from the list",
+			fields: fields{
+				head: func() DoubleLinkedListNode {
+					head := NewList().head
+					head.Next = &node
+					head.Prev = &node
+					return head
+				},
+				length: 1,
+			},
+			expectedLength:   0,
+			expectedKeyPairs: []KeyPair{},
+			args: args{
+				node: &node,
+			},
+		},
+		{
+			name: "removes node from list",
+			fields: fields{
+				head:   setup,
+				length: 2,
+			},
+			args: args{
+				node: setup().Next,
+			},
+			expectedLength:   1,
+			expectedKeyPairs: []KeyPair{{Key: 2, Val: 6}},
+		},
+		{
+			name: "removes middle node from list",
+			fields: fields{
+				head: func() DoubleLinkedListNode {
+					list := NewList()
+
+					list.Insert(KeyPair{Key: 5, Val: 5})
+
+					// 5 -> 2
+					list.Insert(KeyPair{Key: 2, Val: 2})
+
+					// 1 -> 5 -> 2
+					list.InsertAtHead(&node)
+
+					// 7 -> 1 -> 5 -> 2
+					list.InsertAtHead(&node7)
+
+					return list.head
+				},
+				length: 4,
+			},
+			args: args{
+				// remove 1 so result should be: 7 -> 5 -> 2
+				node: &node,
+			},
+			expectedLength:   3,
+			expectedKeyPairs: []KeyPair{{Key: 7, Val: 7}, {Key: 5, Val: 5}, {Key: 2, Val: 2}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &DoubleLinkedList{
+				head:   tt.fields.head(),
+				length: tt.fields.length,
+			}
+			l.Remove(tt.args.node)
+
+			if tt.expectedLength != l.length {
+				t.Errorf("length after remove is not equal! got: %v, want: %v", l.length, tt.expectedLength)
+			}
+
+			keyPairs := l.ToList()
+
+			if !reflect.DeepEqual(keyPairs, tt.expectedKeyPairs) {
+				t.Errorf("key pairs not equal! got: %v, want: %v", keyPairs, tt.expectedKeyPairs)
+			}
+
+		})
+	}
+}
+
+func TestDoubleLinkedList_InsertAtHead(t *testing.T) {
+	type args struct {
+		node *DoubleLinkedListNode
+	}
+	tests := []struct {
+		name             string
+		setup            func() *DoubleLinkedList
+		args             args
+		expectedLength   int
+		expectedKeyPairs []KeyPair
+	}{
+		{
+			name:  "inserts at head of empty list",
+			setup: NewList,
+			args: args{
+				node: &DoubleLinkedListNode{
+					Val: KeyPair{Key: 1, Val: 1},
+				},
+			},
+			expectedLength:   1,
+			expectedKeyPairs: []KeyPair{{Key: 1, Val: 1}},
+		},
+		{
+			name: "inserts into list with some values",
+			setup: func() *DoubleLinkedList {
+				l := NewList()
+				l.Insert(KeyPair{Key: 2, Val: 2})
+				return l
+			},
+			args: args{
+				node: &DoubleLinkedListNode{
+					Val: KeyPair{Key: 1, Val: 1},
+				},
+			},
+			expectedLength:   2,
+			expectedKeyPairs: []KeyPair{{Key: 1, Val: 1}, {Key: 2, Val: 2}},
+		},
+		{
+			name: "the ultimate daddy test",
+			setup: func() *DoubleLinkedList {
+				l := NewList()
+				l.Insert(KeyPair{Key: 4, Val: 4})
+				l.Insert(KeyPair{Key: 3, Val: 3})
+				l.Insert(KeyPair{Key: 2, Val: 2})
+				l.Insert(KeyPair{Key: 1, Val: 1})
+
+				// 4 -> 3 -> 2 -> 1
+
+				return l
+			},
+			args: args{
+				node: &DoubleLinkedListNode{
+					Val: KeyPair{Key: 5, Val: 5},
+				},
+			},
+			expectedLength:   5,
+			expectedKeyPairs: []KeyPair{{Key: 5, Val: 5}, {Key: 4, Val: 4}, {Key: 3, Val: 3}, {Key: 2, Val: 2}, {Key: 1, Val: 1}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := tt.setup()
+			l.InsertAtHead(tt.args.node)
+
+			keyPairs := l.ToList()
+
+			reverseList := l.GetReverseList()
+
+			var forwardsList = make([]KeyPair, len(reverseList))
+
+			var j int
+			for i := len(reverseList) - 1; i >= 0; i-- {
+				forwardsList[j] = reverseList[i]
+				j++
+			}
+
+			if !reflect.DeepEqual(keyPairs, forwardsList) {
+				// this error suggests that the tail pointers aren't being setup correctly
+				t.Errorf("forward list does not equal the same list in reverse. keyPairs: %v, reverseList: %v, forwardsList: %v", keyPairs, reverseList, forwardsList)
+			}
+
+			if !reflect.DeepEqual(keyPairs, tt.expectedKeyPairs) {
+				t.Errorf("key pairs not the same! got: %v, want: %v", keyPairs, tt.expectedKeyPairs)
+			}
+
+			if l.length != tt.expectedLength {
+				t.Errorf("length not the same! got: %v, want: %v", l.length, tt.expectedLength)
+			}
+
 		})
 	}
 }
