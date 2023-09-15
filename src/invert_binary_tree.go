0f2629118747eec720dff1d646841ef6e84cf385
Josh Dando <dandobusiness@gmail.com>
2023-09-15 09:10:46 +0100

    easyyyyy

---
+++ src/invert_binary_tree.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+
+	if root == nil {
+		return root
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return root
+	}
+
+	// swap the left and right nodes
+
+	left := root.Left
+	right := root.Right
+
+	root.Left = invertTree(right)
+	root.Right = invertTree(left)
+
+	return root
+}
